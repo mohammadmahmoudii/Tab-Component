8a416600c92734b03becc07ceae29acacff40932
M.Mahmoudi <mohammadmahmoudi2005@gmail.com>
2023-09-09 17:39:01 +0430

    use callback Function in uppdate usestate()

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,38 @@ const tabData = [
 function App() {
   const [tabActive, setTabActive] = useState(1);
   const [user, setUser] = useState({ name: "mohammad" });
+  const [show,setShow]=useState(true);
+  const [count,setCount]=useState(0);
   // const tabActive = 1;
 
   function handelActiveTab(id) {
     setTabActive(id);
     setUser({ name: "Ali", id: id });
     console.log({ user });
+    // setCount(count+1)
+    // setCount(count+1) // just Add one  becuse ausync
+    //uppdatin usestate with caal back function , we use when data dependent previus state 
+    setCount((prevState)=>prevState+1);
+    setCount((prevState)=>prevState+1);
+  }
+
+  function handelShow()
+  {
+    setShow((show)=>!show);
   }
 
   return (
+    <>
+    <div className="btnC">
+    <button 
+    className="btn"
+    onClick={handelShow}
+    >
+      {show?"Hide":"Show"}
+      </button>
+    </div>
+    {
+      show?
     <div className="tab">
       <div className="tab__header ">
         {tabData.map((t) => (
@@ -48,9 +71,13 @@ function App() {
         ))}
       </div>
       <div className="tab__content">
-        {tabData[tabActive - 1].content} __ {user.name}--{user.id}
+        {tabData[tabActive - 1].content} __ {user.name}--{user.id}--{count}
       </div>
     </div>
+    :
+    ""
+    }
+    </>
   );
 }
 
